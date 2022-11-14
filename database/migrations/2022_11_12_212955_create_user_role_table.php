ec26228201463a14498420d7212eb58feb1f45b3
DafneJimenezR <20193tn029@utez.edu.mx>
2022-11-14 00:11:23 -0600

    feat: updated file name and fields  correction

--- database/migrations/2022_11_12_212955_create_user_role_table.php
+++ database/migrations/2022_11_12_212955_create_user_role_table.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserRolesTable extends Migration
+class CreateUserRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +15,19 @@ class CreateUserRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_roles', function (Blueprint $table) {
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->foreignIdFor(User::class)
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+            $table->foreignIdFor(Role::class)
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
             $table->primary(['user_id', 'role_id']);
+
             $table->softDeletes();
             $table->timestamps();
         });
@@ -27,6 +40,6 @@ class CreateUserRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('user_role');
     }
 }
