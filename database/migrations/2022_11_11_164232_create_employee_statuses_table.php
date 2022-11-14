9ef60b1f282d3e7aca2d9796858415ec0692c15a
DafneJimenezR <20193tn029@utez.edu.mx>
2022-11-14 00:36:36 -0600

    feat: updated file name
    employee_statuses

--- database/migrations/2022_11_11_164232_create_employee_statuses_table.php
+++ database/migrations/2022_11_11_164232_create_employee_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmployeeStatusTable extends Migration
+class CreateEmployeeStatusesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateEmployeeStatusTable extends Migration
      */
     public function up()
     {
-        Schema::create('employee_status', function (Blueprint $table) {
+        Schema::create('employee_statuses', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50)->unique();
             $table->string('description')->nullable();
@@ -29,6 +29,6 @@ class CreateEmployeeStatusTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('employee_status');
+        Schema::dropIfExists('employee_statuses');
     }
 }
